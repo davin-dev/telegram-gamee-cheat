0bec9dbc9660d2b5311fa329431ea600d146ae33
A <sweetwow0098@gmail.com>
2017-11-02 17:29:04 +0330

    Update gamee.js

--- gamee.js
+++ gamee.js
@@ -1,21 +1,23 @@
-var url = window.location.pathname,
-        timestamp = (new Date).getTime(),
-        dataId = $("#dataId").data(),
-        hash = CryptoJS.AES.encrypt(JSON.stringify({
-            score: 999999999,  
-            timestamp: timestamp
-        }), dataId.id, {
-            format: CryptoJSAesJson
-        }).toString(),
-        sData = {
-            score: 999999999,
-            url: url,
-            play_time: gameeUI.playTime,
-            hash: hash
-        };
-    if (isFacebook()) {
-        var fbUserData = FacebookUserData.getUserData();
-        sData["app_scoped_user_id"] = fbUserData.app_scoped_user_id;
-        sData["user_id"] = fbUserData.user_id
-    }
-    gameeUI.sendScoreData(sData)
+var a = window.location.pathname,
+            t = gameeUI.user,
+            n = (new Date).getTime(),
+            o = $("#dataId").data(),
+            i = CryptoJS.AES.encrypt(JSON.stringify({
+                score: 9999999999999999999999,
+                timestamp: n
+            }), o.id, {
+                format: CryptoJSAesJson
+            }).toString(),
+            s = {
+                score: 9999999999999999999999,
+                url: a,
+                play_time: gameeUI.playTime,
+                hash: i,
+                username: t,
+                anonymous_id: gameeUI.anonymous_id
+            };
+        if (isFacebook()) {
+            var r = FacebookUserData.getUserData();
+            s.app_scoped_user_id = r.app_scoped_user_id, s.user_id = r.user_id
+        }
+        gameeUI.sendScoreData(s)
