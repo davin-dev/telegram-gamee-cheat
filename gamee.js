68b2c0f795307b0aa3f1616225f9e9a08a263ecd
A <sweetwow0098@gmail.com>
2018-08-21 01:31:29 +0430

    Update gamee.js

--- gamee.js
+++ gamee.js
@@ -1,23 +1,23 @@
-var a = window.location.pathname,
-            t = gameeUI.user,
-            n = (new Date).getTime(),
-            o = $("#dataId").data(),
-            i = CryptoJS.AES.encrypt(JSON.stringify({
-                score: 9999999999999999999999,
-                timestamp: n
-            }), o.id, {
+ var t = void 0 !== gameeUI.saveStateData ? gameeUI.saveStateData : "",
+            a = a || !1,
+            n = window.location.pathname,
+            s = gameeUI.user,
+            r = (new Date).getTime(),
+            i = $("#dataId").data(),
+            o = CryptoJS.AES.encrypt(JSON.stringify({
+                score: 9999999999,
+                timestamp: r,
+                saveState: t
+            }), i.id, {
                 format: CryptoJSAesJson
             }).toString(),
-            s = {
-                score: 9999999999999999999999,
-                url: a,
+            l = {
+                score: 9999999999,
+                url: n,
                 play_time: gameeUI.playTime,
-                hash: i,
-                username: t,
-                anonymous_id: gameeUI.anonymous_id
+                hash: o,
+                username: s,
+                anonymous_id: gameeUI.anonymous_id,
+                game_state_data: t
             };
-        if (isFacebook()) {                     //is not important you can delete it..
-            var r = FacebookUserData.getUserData();
-            s.app_scoped_user_id = r.app_scoped_user_id, s.user_id = r.user_id
-        }
-        gameeUI.sendScoreData(s)
+        a ? gameeUI.sendScoreData(l) : gameeUI.sendScoreDataGameover(l)
