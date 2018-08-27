2e864bca69ed7d0b217a6cc8cef5fc7b33bb97b2
A <sweetwow0098@gmail.com>
2018-08-28 01:27:58 +0430

    Update gamee.js

--- gamee.js
+++ gamee.js
@@ -1,18 +1,18 @@
- var t = void 0 !== gameeUI.saveStateData ? gameeUI.saveStateData : "",
+        var t = void 0 !== gameeUI.saveStateData ? gameeUI.saveStateData : "",
             a = a || !1,
             n = window.location.pathname,
             s = gameeUI.user,
             r = (new Date).getTime(),
             i = $("#dataId").data(),
             o = CryptoJS.AES.encrypt(JSON.stringify({
-                score: 9999999999,
+                score: e,
                 timestamp: r,
                 saveState: t
             }), i.id, {
                 format: CryptoJSAesJson
             }).toString(),
             l = {
-                score: 9999999999,
+                score: e,
                 url: n,
                 play_time: gameeUI.playTime,
                 hash: o,
@@ -20,4 +20,8 @@
                 anonymous_id: gameeUI.anonymous_id,
                 game_state_data: t
             };
+        if (!isTelegram() && !isKik() && (isFacebook() || FacebookUserData && FacebookUserData.isLoggedIn() && !isViber())) {
+            var c = FacebookUserData.getUserData();
+            l.facebook_user_id = c.app_scoped_user_id, null !== gameGlobals.facebookThreadId && (l.facebook_thread_id = gameGlobals.facebookThreadId), l.user_id = c.user_id
+        }
         a ? gameeUI.sendScoreData(l) : gameeUI.sendScoreDataGameover(l)
